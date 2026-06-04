c84301025601c93356e6f2e3d18f7ef523de2f87
Vincent Ilagan <vfxverse25@gmail.com>
2026-06-04 10:39:43 +0400

    Delete Run_AfterCut.bat

--- Run_AfterCut.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "PYTHON=C:\Users\Richmindale\.cache\codex-runtimes\codex-primary-runtime\dependencies\python\python.exe"
-set "APP_DIR=%~dp0"
-
-"%PYTHON%" "%APP_DIR%AfterCut.py"
-if errorlevel 1 pause
